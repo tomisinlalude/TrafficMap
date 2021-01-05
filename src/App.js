d0613d47de1ae50a4439b6e1019010bcd89c76dc
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-01-05 18:07:19 +0100

    Map component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
+import {Container} from 'reactstrap';
 import Header from "./components/Header";
+import Map from "./components/Map";
+
+require('dotenv').config()
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -10,8 +15,11 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div>
         <Header appName={this.state.applicationName}/>
+        <Container>
+          <Map />
+        </Container>
       </div>
     )
   }
