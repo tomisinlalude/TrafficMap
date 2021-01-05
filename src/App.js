eeb02746a97bd8e7d16f9bdf2d5e774c4aa9c15b
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-01-05 17:36:48 +0100

    Put Header in its component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,17 @@
 import React, { Component } from "react";
-import './App.css';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem
-} from 'reactstrap'; 
-
+import Header from "./components/Header";
 class App extends Component {
   constructor(props) {
     super(props);
-
-    this.toggle = this.toggle.bind(this);
     this.state = {
-      isOpen: false,
+      applicationName: 'TrafficMap'
     }
-  }
-
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    })
   }
 
   render() {
     return (
       <div className="App">
-        <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
-        <NavbarToggler onClick={this.toggle} />
-        <Collapse isOpen={this.state.isOpen} navbar>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              1,000 Traffic Incidents
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
+        <Header appName={this.state.applicationName}/>
       </div>
     )
   }
---
+++ src/components/Header.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem
+} from 'reactstrap'; 
+
+export default class Header extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+          isOpen: false,
+        }
+      }
+    
+      toggle() {
+        this.setState({
+          isOpen: !this.state.isOpen
+        })
+    }
+
+    render() {
+        return (
+        <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">{this.props.appName}</NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              1,000 Traffic Incidents
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+      )
+    }
+}
