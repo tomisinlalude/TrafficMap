ba88fac0ae5eb1673b4bc94b583f098caab553c6
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-01-05 20:31:27 +0100

    Use data instead of dummy array

--- src/components/Map.js
+++ src/components/Map.js
@@ -7,6 +7,7 @@ export default class Map extends Component {
         super(props);
 
         this.state = {
+            api_url: 'https://data.edmonton.ca/resource/87ck-293k.json',
             viewport:  {
             width: 1100,
             height: 600,
@@ -18,19 +19,30 @@ export default class Map extends Component {
                 {latitude: 53.5225, longitude: -113.6242},
                 {latitude: 53.5437, longitude: -113.4917},
                 {latitude: 53.5439, longitude: -113.4914},
-            ]
+            ],
+            data: null
+        }
+    }
+
+    componentDidMount() {
+        const {data, api_url} = this.state;
+
+        if (!data) {
+            fetch(api_url, {method: 'GET' })
+            .then(res => res.json())
+            .then(res => console.log(res))
         }
     }
 
     render() {
-        const { coords } = this.state;
+        const { coords, data } = this.state;
         return (
             <MapGL 
             mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
             {...this.state.viewport}
             onViewportChange={(viewport) => this.setState({viewport})}>
-                {coords.map(coord => (
-                <Marker latitude={coord.latitude} longitude={coord.longitude}>
+                {coords.map((coord, i) => (
+                <Marker key={`Marker-${i * (Math.random() * 200 + 1)}`} latitude={coord.latitude} longitude={coord.longitude}>
                     <Pin />
                 </Marker>
                 ))}
