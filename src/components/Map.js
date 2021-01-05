fdf4facdad83ec21bfa78e1f94de08521430f27a
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-01-05 21:23:56 +0100

    Refactor Map component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,25 +1,16 @@
 import React, { Component } from "react";
-import MapGL, { Marker } from 'react-map-gl';
-import Pin from './Pin';
-
+import MapboxGL from 'mapbox-gl';
 export default class Map extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
             api_url: 'https://data.edmonton.ca/resource/87ck-293k.json',
+            map: false,
             viewport:  {
-            width: 1100,
-            height: 600,
             zoom: 10,
-            latitude: 53.5444,
-            longitude: -113.4909
+            center: [ -113.4909, 53.5444 ]
             },
-            coords: [
-                {latitude: 53.5225, longitude: -113.6242},
-                {latitude: 53.5437, longitude: -113.4917},
-                {latitude: 53.5439, longitude: -113.4914},
-            ],
             data: null
         }
     }
@@ -34,21 +25,24 @@ export default class Map extends Component {
         }
     }
 
+    initializeMap() {
+        MapboxGL.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
+        let map = new MapboxGL.Map({
+           container: "map",
+           style: 'mapbox://styles/mapbox/light-v9',
+           ...this.state.viewport
+        })
+
+        this.setState({ map });
+    }
+
     render() {
-        const { coords, data } = this.state;
+        const { data, map } = this.state;
+
+        if (data && !map) this.initializeMap();
+
         return (
-            <MapGL 
-            mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-            {...this.state.viewport}
-            onViewportChange={(viewport) => this.setState({viewport})}>
-                {coords.map((coord, i) => (
-                <Marker key={`Marker-${i * (Math.random() * 200 + 1)}`} latitude={coord.latitude} longitude={coord.longitude}>
-                    <Pin />
-                </Marker>
-                ))}
-                
-            </MapGL>
-            
+            <div style={{width:1100, height: 600}} id="map"></div>
         );
     }
 }
